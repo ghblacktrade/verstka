5a3db721080a6f3b21ecfe3c2cdfc43b2bb11e29
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2023-03-26 21:19:52 +0700

    new-cars

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,30 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item')
+const tabContent = document.querySelectorAll('.tabs__content-item')
+
+tabItem.forEach(function (element) {
+    element.addEventListener('click', open)
+})
+
+function open(evt) {
+    const tabTarget = evt.currentTarget
+    const btn = tabTarget.dataset.button
+
+    tabItem.forEach((item) => {
+        item.classList.remove('tabs__btn-item--active')
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active')
+
+    tabContent.forEach((item) => {
+        item.classList.remove
+        ('tabs__content-item--active')
+    })
+
+    document.querySelector(`#${btn}`)
+        .classList.add('tabs__content-item--active')
+}
+
+
 const swiper = new Swiper('.swiper', {
     effect: 'fade',
     pagination: {
